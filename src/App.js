e6fd4f46a63489f377275865a0fd754c7a610383
Mike Robles <mikej.robles@gmail.com>
2023-01-25 17:30:40 -0500

    removed redundant code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './App.css'
 import StudentData from "./StudentData"
 
 function App() {
--- src/Student.jsx
+++ src/Student.jsx
@@ -7,7 +7,7 @@ const Student = (props) => {
       <h4>{props.student.bio}</h4>
       <h5>
         {props.student.scores.map((score, idx) =>
-        <Score score={score}  key={idx} studentName={props.student.name}/>
+        <Score score={score} key={idx} />
         )}
       </h5>
     </>
